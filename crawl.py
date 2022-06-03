f4058586a904c4e3ae17f8eb4b1f99bc4a47ef34
Alireza Hadjar <asiahadjar@gmail.com>
2022-06-04 00:20:09 +0430

    initial commit

---
+++ crawl.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+from newspaper import Article
+from tqdm import tqdm
+import pandas
+
+base_url = "https://www.bartarinha.ir"
+def get_page_url (page):
+    return f"{base_url}/بخش-اخبار-سیاسی-24?curp=1&categories=24&dateRange%5Bstart%5D=-604800&order=order_time&page={page}"
+def get_data (url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    text, title = article.text, article.title
+
+    return {"url": url, "text": text, "title": title}
+
+def crawl ():
+    data = []
+    page = 15
+    finished = False
+
+    while True:
+        page = page + 1
+        url = get_page_url(page)
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, features="lxml")
+        lists = soup.find_all("ul", {"class": "main_land_list"})
+
+        if finished: break
+
+        for list in lists:
+            links = list.find_all("li")
+            if len(links) == 0: finished = True
+            for link in tqdm(links):
+                url = base_url + link.h3.a['href']
+                try:
+                    data.append(get_data(url))
+                except:
+                    print(f"Failed to load {url}")
+    
+    df = pandas.DataFrame(data)
+    df.to_csv(f"crawled.csv")
+
+
+crawl()
